257cef053838b9a72263242777325f35b037fb86
Jesus Plasencia Toledo <jesusplasencia2508@hotmail.com>
2022-10-24 09:33:19 -0500

    Basic 02 - JS

---
+++ Lectures/Intro/elementary.js
@@ -0,0 +1,21 @@
+// !Valores Primitivos
+// !Numbers
+40
+
+// !Strings
+"Jesus"
+
+// !Booleans
+false
+true
+
+// !Empty values:
+null
+undefined
+
+// !Valores No Primitivos o Valores Tipo Objetos
+// !Arrays
+[1, 2, 3]
+
+// !Objetos
+{ nombre: "Jesus" }
