2ef1678859bf5a0682acdff60a783493216c093b
Jesus Plasencia Toledo <jesusplasencia2508@hotmail.com>
2022-10-24 09:32:26 -0500

    Basic 01 - JS

---
+++ Lectures/basic/functions.js
@@ -0,0 +1,7 @@
+// !Declarativas
+function miFuncion() {
+    return 3;
+}
+
+// !Expresivas
+var miFuncion = function name(params) { return 4;}
---
+++ Lectures/basic/variables.js
@@ -0,0 +1,33 @@
+// !Variables
+
+// !VAR
+// !Declaracion
+// var edad;
+// !Inicializacion
+// edad = 21;
+// !Mostrar Valor
+// console.log("Edad:", edad);
+// !Usando su valor en una funcion
+// function usingVarInsideAFunction() {
+//     return edad * 10;
+// }
+// console.log("Usando una var:", usingVarInsideAFunction());
+// console.log("Usando la var de la funcion:", edad);
+
+// !LET
+// !Declaracion
+// let promedio;
+// !Inicializacion
+// promedio = 19.5;
+// !Mostrar Valor
+// console.log("Promedio:", promedio);
+// !Usando su valor en una funcion
+// function usingLetInsideAFunction() {
+//     return promedio * 10;
+// }
+// console.log("Usando un let en la funcion:", usingLetInsideAFunction());
+
+// !CONST
+// !Declaracion e Inicializacion
+// const MY_AGE = 21;
+// console.log(MY_AGE);
